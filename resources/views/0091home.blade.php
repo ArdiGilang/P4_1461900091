b5bf62e172489567d121f09ab00508ccc95690c8
ArdiGilang <gilangardi741@gmail.com>
2021-06-12 13:45:12 +0700

    first commit

---
+++ resources/views/0091home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+	<title>Home</title>
+</head>
+<div id="header" class="header">
+		<div id="search">
+</div>
+        <h1>Ini Home</h1>
+		<li class="first"><a href="/buku">lihat</a></li>
+
+</body>
+</html>
